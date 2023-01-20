4cf69564fbbcf847f57da3eb2ec535fff893b3a6
Natalia-Bogatyreva <rogova_nu@mail.ru>
2023-01-20 10:15:56 +0500

    Задание по JavaScript

--- JavaScript/name.js
+++ JavaScript/name.js
@@ -3,7 +3,6 @@
   let adminName = "Наталья Богатырева";
   var userName = prompt ("Введите свое имя ", "Имя");
   var userSur = prompt ("Введите свою фамилию ", "Фамилия");
-  var txt=("");
   if (adminName == (userName+" "+userSur))
       {
           alert("Ты - автор программы. Так не считается. Позови другого пользователя!");
